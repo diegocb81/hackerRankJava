9104e2ee4b0b6cdb6cfaa3b2def09334473eb4a3
Diego Barcellos <diegocb81@gmail.com>
2024-03-31 23:59:04 -0300

    Day 7 OK

---
+++ src/application/Day7.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Day7 {
+	
+	public class Solution {
+	    public static void main(String[] args) throws IOException {
+	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+	        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+	        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+	                .map(Integer::parseInt)
+	                .collect(toList());
+	        printReverse(arr);
+	        bufferedReader.close();
+	    }
+
+	    public static void printReverse(List<Integer> arr) {
+	        for (int i = arr.size() - 1; i >= 0; i--)
+	            System.out.print(arr.get(i) + " ");
+	    }
+	}
+
+	
+
+}
