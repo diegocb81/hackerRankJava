c2a0ee05c51623909704c929c5b66e562ff909fc
Diego Barcellos <diegocb81@gmail.com>
2024-04-15 02:02:33 -0300

    Adjustment made to the Calculadora class on day 19

--- src/application/Day19.java
+++ src/application/Day19.java
@@ -1,14 +1,12 @@
 package application;
 
-import java.io.*;
 import java.util.*;
-import java.util.Scanner;
 
 interface AdvancedArithmetic {
     int divisorSum(int n);
 }
 
-class Calculator implements AdvancedArithmetic {
+class Calculadora implements AdvancedArithmetic {
     public int divisorSum(int n) {
         int sum = 0;
         for (int i = n; i > 0; i--) {
@@ -20,14 +18,13 @@ class Calculator implements AdvancedArithmetic {
     }
 }
 
-public class Day19 {
-
+class Day19 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         scanner.close();
 
-        AdvancedArithmetic myCalculator = new Calculator();
+        AdvancedArithmetic myCalculator = new Calculadora();
         int sum = myCalculator.divisorSum(n);
         System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName());
         System.out.println(sum);
